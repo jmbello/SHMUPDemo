3bcd0417c6a50433eac8758faee74fa5a92d25ab
jbello <jorgebell@gmail.com>
2018-09-06 11:05:27 -0400

    First commit

---
+++ Assets/_Scripts/GamePlay/HealthSystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour {
+
+    private string tagName;
+    private float currentHealth;
+
+    public bool isEnemy = true;
+    public float maxHealth = 10f;
+    public GameObject hitEffect;
+    public GameObject healthBar;
+
+
+	// Use this for initialization
+	void OnEnable () {
+        if (!isEnemy)
+        {
+            tagName = "Bullet";
+        }
+        else
+        {
+            tagName = "EnemyBullet";
+        }
+        currentHealth = maxHealth;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(tagName))
+        {
+            Vector3 triggerPosition = 
+                other.ClosestPointOnBounds(transform.position);
+            Vector3 direction = 
+                triggerPosition - transform.position;
+            GameObject fx = PoolingManager.instance.UseObject
+                (hitEffect, triggerPosition, Quaternion.LookRotation(direction));
+            PoolingManager.instance.ReturnObject(fx, 1f);
+
+            // do damage here
+            float damage = float.Parse(other.name);
+            TakeDamage(damage);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+    }
+}
---
+++ Assets/_Scripts/PoolingSystem/PoolingManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingManager : MonoBehaviour {
+
+    public static PoolingManager instance;
+
+    private void Awake()
+    {
+        print("PoolingManager is awake");
+        if (!instance)
+        {
+            instance = this;
+        }
+    }
+
+    public GameObject UseObject(GameObject obj, Vector3 pos, Quaternion rot)
+    {
+        return Instantiate(obj, pos, rot);
+    }
+
+    public void ReturnObject(GameObject obj, float delay = 0f)
+    {
+        Destroy(obj, delay);
+    }
+}
